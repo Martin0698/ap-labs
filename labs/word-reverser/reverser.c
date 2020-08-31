35da2736bb8be9b00f300709ab10e9782ef8faaf
Martin Guevara <A01632780qitesm.mx>
2020-08-31 02:31:35 +0000

    Solve word-reverser
    Signed-off-by: Martin Guevara <A01632780qitesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void invertir(char *caracteres, int tamanno){
+    int ptamano= tamanno;
+    char *invertido= malloc( tamanno* sizeof(char));
+    for (int i = tamanno-1; i >= 0; i--)
+    {
+        invertido[tamanno-(i+1)]= caracteres[i];
+        /* code */
+    }
+
+    for (int i = 0; i < ptamano; i++)
+    {
+        printf("%c", invertido[i]);
+        /* code */
+    }
+    printf("\n");
+    
+    
+}
 
 int main(){
-    // Place your magic here
+    int caracter;
+    char letras[60];
+    int tamano= 0;
+
+    while ((caracter=getchar()) != EOF)
+    {
+        /* code */
+        
+        if (caracter != '\n')
+        {
+            letras[tamano++]= caracter;
+            /* code */
+            //printf("%c", caracter);
+
+        }
+        else
+        {
+            /* code */
+            invertir(letras, tamano);
+            tamano=0;
+        }
+        
+    }
+    
     return 0;
 }
+
