1f7faf6ec80d9e2fdc176eabb5c651027a4fb286
Martin Guevara <A01632780@itesm.mx>
2020-09-08 21:37:29 +0000

    Solve str-lib
    Signed-off-by: Martin Guevara <A01632780@itesm.mx>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include "strlib.c"
 
-int main() {
+
+int main(int argc, char *argv[]) {
+    
+    if(argc == 4){
+
+    
+      
+         if(strcmp(argv[1], "-add")== 0)
+         {
+             
+            int tm = mystrlen(argv[2]);
+            char *nuevo= mystradd(argv[2], argv[3]);
+
+            printf("tamanio inicial: %i \n", tm);
+            printf("string concatenado: %s \n", nuevo);
+         }
+
+         else if (strcmp(argv[1], "-find")== 0)
+         {
+             printf("El string se encontro [%s]   en la posicion [%d]\n", argv[3],mystrfind(argv[2],argv[3]));
+  
+         }
+         
+
+    }
+    
+    else
+    {
+        printf("tiene un exceso o menos entradas de las requeridas \n");
+    }
+    
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,61 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int tamano=0;
+
+    while (str[tamano] != '\0')
+    {
+        tamano++;
+    }
+    
+    return tamano;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+
+    char *nuevo = malloc(mystrlen(origin)+ mystrlen(addition));
+
+    int tamanio1 = mystrlen(origin);
+
+    int tamanio2 = mystrlen(addition);
+
+    for (int i = 0; i < tamanio1; i++)
+    {
+        nuevo[i] = origin[i];
+    }
+
+    int i = 0;
+
+    for (int j = tamanio1; j < tamanio2 + tamanio1; j++)
+    {
+        nuevo[j] = addition[i];
+        
+        i++;
+    }
+    return nuevo;
+    
+    
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
-}
+    int limit = mystrlen(substr);
+
+    int taminioorigen = mystrlen(origin);
+    
+    int counter = 0;
+    
+    for(int i = 0;i < taminioorigen;i++){
+
+      if(*(origin+i) == *(substr+counter)){
+        counter++;
+      }
+      else {
+        i = i - counter + 1;
+        counter = 0;
+      }
+      if(counter==limit) return i - counter + 1;
+    }
+    return -1;
+}	
